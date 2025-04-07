1b765f11220cfbe917da28f7d93f6a2ec15b5de5
Maria Aida Fernandes Vitoria <mariaafv@hotmail.com>
2025-04-07 19:08:17 -0300

    Initial Commit

---
+++ sushi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  sushi
+//
+//  Created by Maria Aida Fernandes Vitoria - Unicred do Brasil on 07/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sushi/sushiApp.swift
@@ -0,0 +1,17 @@
+//
+//  sushiApp.swift
+//  sushi
+//
+//  Created by Maria Aida Fernandes Vitoria - Unicred do Brasil on 07/04/25.
+//
+
+import SwiftUI
+
+@main
+struct sushiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
